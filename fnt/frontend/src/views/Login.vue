bc43ea6eb449b21b8fb4d40a4953ca022e13bed5
edissonpct <eddp29@gmail.com>
2026-04-06 23:16:13 -0500

    inicio pruebas frontend

---
+++ fnt/frontend/src/views/Login.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+const handleSubmit = () => {} 
+</script>
+
+<template>
+    <h1>LOGIN PAGE</h1>
+    <form @submit.prevent="handleSubmit" class="form">
+        <label for="username">Usuario</label>
+        <input type="text" id="username">
+
+        <label for="password">Contraseña</label>
+        <input type="text" id="password">
+        <button>Iniciar sesión</button>
+    </form>
+</template>
+
+<style>
+    .form {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+</style>
