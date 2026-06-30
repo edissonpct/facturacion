8120319aff6cb8399bfeba872f9d1ea5af7e2aec
edissonpct <eddp29@gmail.com>
2026-06-29 22:28:23 -0500

    Mejora UI

--- fnt/frontend/src/components/HomeHeader.vue
+++ fnt/frontend/src/components/HomeHeader.vue
@@ -1,20 +1,34 @@
 <script setup>
 import Login from '../views/Login.vue';
+import { Toolbar, Button, Avatar } from 'primevue';
+
 
 </script>
 <template>
-    <header class="header">
-        <h1>Nunaq</h1>
-        <nav>
-            <a href="/">Inicio</a>
-            <a> 
-                <router-link :to = "{name: 'Plan'}">Planes y precios</router-link>
-            </a>
-            <a> 
-                <router-link :to = "{name: 'Login'}">Iniciar Sesión</router-link>
-            </a>
-        </nav>
-    </header>
+    <Toolbar style="border-radius: 3rem; padding: 1rem 1rem 1rem 1.5rem">
+        <template #start>
+            <div class="flex items-center gap-2">
+                <img src="/src/assets/logo.png" alt="Logo" class="h-10 w-auto" />
+                <router-link :to="{ name: 'Home' }">
+                    <Button label="Inicio" text plain />
+                </router-link>
+                <router-link :to="{ name: 'Plan' }">
+                    <Button label="Planes y precios" text plain />
+                </router-link>
+                <Button label="Acerca" text plain />
+                
+            </div>
+        </template>
+
+        <template #end>
+            <div class="flex items-center gap-2">
+                <router-link :to="{ name: 'Login' }">
+                    <Button label="Iniciar sesión" severity="contrast" size="small" />
+                </router-link>
+                <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" style="width: 32px; height: 32px" />
+            </div>
+        </template>
+    </Toolbar>
 </template>
 <style scoped>
 .header {
--- fnt/frontend/src/views/Home.vue
+++ fnt/frontend/src/views/Home.vue
@@ -1,12 +1,15 @@
 <script setup>
-import HomeHeader from '../components/HomeHeader.vue';
-import HomeFooter from '../components/HomeFooter.vue';
+import HomeHeader from '../components/HomeHeader.vue'
+import HomeFooter from '../components/HomeFooter.vue'
+import Button from 'primevue/button'
 </script>
 
 <template>
   <div class="layout">
     <HomeHeader />
-
+    <div class="flex justify-center items-center h-screen">
+      <Button label="Guardar" icon="pi pi-check" />
+    </div>
     <main class="content">
       <h2>Bienvenido al Home</h2>
       <p>Este es el contenido principal</p>
